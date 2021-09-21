d69ea17355a6e8ae9540fa210bab2d69b0bf2802
Mathis <Mathis.Strohfeldt@hotmail.com>
2021-09-22 00:35:12 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,4 +4,19 @@
 import tkinter as tk
 
 app = tk.Tk()
+app.geometry("400x300")
+app.title("test")
+
+def calcTest():
+	number = e1.get()
+	print(number)
+	e1.delete(0, "end")
+
+l1 = tk.Label(app, text="Enter first number")
+l1.pack()
+e1 = tk.Entry(app)
+e1.pack()
+b1 = tk.Button(text="calculate", command=calcTest)
+b1.place(x=10, y=10)
+
 app.mainloop()
